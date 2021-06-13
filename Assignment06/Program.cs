b7d1c33c9986acaafba9cadccc4913db134f5708
Thiem <tranthiem1993@gmail.com>
2021-06-13 10:04:21 +0700

    Assignment06

---
+++ Assignment06/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Harry Potter", "123456789", "J.K.Rowling", "Kim Dong");
+            Console.WriteLine("ISBN Number\tBook Name\tAuthor Name\tPublisher Name");
+            Console.WriteLine(book.GetBookInformation());
+            Console.ReadKey();
+        }
+    }
+}
